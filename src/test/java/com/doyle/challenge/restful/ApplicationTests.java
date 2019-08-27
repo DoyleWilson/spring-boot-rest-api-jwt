e342f118089c80a6db40a6b820464e2727e4da8f
Doyle Wilson <doyle4u@gmail.com>
2019-08-28 03:07:43 +1000

    Test Cases - commented out to incorporate access token

--- src/test/java/com/doyle/challenge/restful/ApplicationTests.java
+++ src/test/java/com/doyle/challenge/restful/ApplicationTests.java
@@ -5,11 +5,35 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import org.mockito.Mock;
+import org.springframework.test.web.servlet.MockMvc;
+import com.doyle.challenge.restful.controller.EmployeeAPI;
+
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApplicationTests {
+	
+	@Mock
+	private EmployeeAPI empRepo;
+
+	private MockMvc mockMvc;
 
 	@Test
 	public void contextLoads() {
 	}
+	
+	/*
+	 * @Test public void testUpdateEmployee() throws Exception { Employee emp = new
+	 * Employee(); emp.setId((long) 1); emp.setFullName("Wilson Doyle");
+	 * 
+	 * ObjectMapper mapper = new ObjectMapper(); String jsonString =
+	 * mapper.writeValueAsString(emp); Mockito.when(((OngoingStubbing)
+	 * empRepo.retrieveEmployees((Mockito.anyLong()))).thenReturn(emp));
+	 * Mockito.doNothing().when(empRepo).updateEmployees(Mockito.any(Employee.class)
+	 * , 1); mockMvc.perform(
+	 * MockMvcRequestBuilders.put("/employees/1").content(jsonString).contentType(
+	 * MediaType.APPLICATION_JSON))
+	 * .andExpect(MockMvcResultMatchers.status().is(401)); }
+	 */
 }
